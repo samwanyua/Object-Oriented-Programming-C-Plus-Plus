48f3c1e1e604ea7027b14d6e3e7fd04ab9aa5058
samwanyua <samexample8@gmail.com>
2024-02-26 19:55:00 +0300

    Classes and Objects

--- oop.cpp
+++ oop.cpp
@@ -11,12 +11,27 @@ class Person{
     }
 };
 
+/*
+class -describes the structure
+object - a specific example of that structure (instance)
+instance is another name for object
+creating objects from a class - is instantiating
+
+methods - functions defined inside a class
+*/
+
 int main(){
-    
+
     Person person1;
     person1.first = "Caleb";
     person1.last = "Cate";
 
     person1.printFullName();
+
+    Person p2;
+    p2.first = "Sam";
+    p2.last = "Wanyua";
+
+    p2.printFullName();
     return 0;
 }
